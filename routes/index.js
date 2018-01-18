b60b7d42f038702e86a9b9bd6f9cf95b5a8b8e7c
Manish Dwibedy <manish.dwibedy@gmail.com>
2018-01-18 10:06:24 -0800

    getting the start/stop working.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
+
+function myFunc(arg) {
+    console.log('calling myFunc with args - ' + arg);
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+
+var timerID = ''
+
+/* GET home page. */
+router.get('/start', function(req, res, next) {
+    this.timerID = setInterval(myFunc, 2000);
+    res.render('index', { title: 'Express' });
+});
+
+/* GET home page. */
+router.get('/stop', function(req, res, next) {
+    clearInterval(this.timerID);
+    res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
