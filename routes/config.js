e569fc08f7330fc515b5898413fd3d096c716e8c
Manish Dwibedy <manish.dwibedy@gmail.com>
2018-01-19 12:45:50 -0800

    remove comments.

--- routes/config.js
+++ routes/config.js
@@ -4,13 +4,4 @@ module.exports = {
     consumer_key: 'NK6l2E4jgjybsMTH4A7Ur9RGs',
     consumer_secret: 'mVCMgE0UfRRXEqvYy37MyPRFOSAWvi3ClQbS8sUiZPm2Acv8VL',
     bearer_token: ''
-}
-
-
-//     {
-//     consumer_key: 'ljcXWxF6KObOeFzbkMk5hYdSy',
-//     consumer_secret: 'go0KUUZ5ppFdT6g6rW2iWEo2KaoOBxE4zn2X26j0vyz39WwGOm',
-//     access_token_key: '920035559363395584-ezx1HsKr3QqLpNSPgV5jawmbjLIDIoc',
-//     access_token_secret: 'Z3Y4ixcR5VFI0UAC05XakNnTlFtTmanYcRdOz03lmALR6'
-// }
-
+};
