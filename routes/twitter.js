d6c19c9d093c4fec8c3b30ad459bb7f420c8362a
Manish Dwibedy <manish.dwibedy@gmail.com>
2018-01-19 18:32:10 -0800

    now, using user-level auth, that is needed for replying to an user's tweet.

--- routes/twitter.js
+++ routes/twitter.js
@@ -4,26 +4,14 @@ var Twitter = require('twitter');
 const getBearerToken = require('get-twitter-bearer-token')
 
 var config = require('./config.js');
-var T = null;
+var T = new Twitter(config);
 
-getBearerToken(config.consumer_key, config.consumer_secret, function(err, response) {
-    if(!err){
-        config.bearer_token = response.body.access_token;
-
-        this.T = new Twitter(config);
-        // Loop through the returned tweets
-
-    } else {
-        console.log(err);
-    }
-});
-
-var searchTweets = function(params) {
+var reply  = function(params) {
     // Return new promise
     return new Promise(function(resolve, reject) {
 
         // Do async job
-        this.T.get('search/tweets', params, function(err, data, response) {
+        T.post('statuses/update', params, function(err, data, response) {
             if(!err){
                 var json = JSON.parse(response.body);
                 resolve(json);
@@ -35,5 +23,55 @@ var searchTweets = function(params) {
         })
     })
 };
+
+
+var searchTweets = function(params) {
+    // Return new promise
+    return new Promise(function(resolve, reject) {
+
+        // Do async job
+        T.get('search/tweets', params, function(err, data, response) {
+            if(!err){
+                var json = JSON.parse(response.body);
+                resolve(json);
+                // This is where the magic will happen
+            } else {
+                console.log(err);
+                reject(err);
+            }
+        })
+    })
+};
+
+
+// getBearerToken(config.consumer_key, config.consumer_secret, function(err, response) {
+//     if(!err){
+//         config.bearer_token = response.body.access_token;
+//
+//         this.T = new Twitter(config);
+//         // Loop through the returned tweets
+//
+//     } else {
+//         console.log(err);
+//     }
+// });
+//
+// var searchTweets = function(params) {
+//     // Return new promise
+//     return new Promise(function(resolve, reject) {
+//
+//         // Do async job
+//         this.T.get('search/tweets', params, function(err, data, response) {
+//             if(!err){
+//                 var json = JSON.parse(response.body);
+//                 resolve(json);
+//                 // This is where the magic will happen
+//             } else {
+//                 console.log(err);
+//                 reject(err);
+//             }
+//         })
+//     })
+// };
 
 module.exports.searchTweets = searchTweets;
