8b8a4e55cd0f4afa08542b0c5f8f6578777874b7
Manish Dwibedy <manish.dwibedy@gmail.com>
2018-01-19 17:33:53 -0800

    now relying to tweet works as well.

--- routes/config.js
+++ routes/config.js
@@ -3,5 +3,7 @@
 module.exports = {
     consumer_key: 'NK6l2E4jgjybsMTH4A7Ur9RGs',
     consumer_secret: 'mVCMgE0UfRRXEqvYy37MyPRFOSAWvi3ClQbS8sUiZPm2Acv8VL',
-    bearer_token: ''
+    bearer_token: '',
+    access_token_key: '920035559363395584-ezx1HsKr3QqLpNSPgV5jawmbjLIDIoc',
+    access_token_secret: 'Z3Y4ixcR5VFI0UAC05XakNnTlFtTmanYcRdOz03lmALR6'
 };
--- test-twitter.js
+++ test-twitter.js
@@ -5,59 +5,59 @@ var config = require('./routes/config.js');
 var T = null;
 
 
-getBearerToken(config.consumer_key, config.consumer_secret, function(err, response) {
-    if(!err){
-        config.bearer_token = response.body.access_token;
-
-        this.T = new Twitter(config);
-
-        var replyTo = {
-            status: "👋",
-            in_reply_to_status_id: 954498162902949889,
-            auto_populate_reply_metadata: true
-        }
-        reply(replyTo).then(function(result) {
-            console.log(result);
-        });
-
-        var la_search = {
-            q: 'roommate',
-            count: 100,
-            result_type: 'recent',
-            lang: 'en',
-            max_id: 954497154877358079
-            // since_id: 9007199254740991
-            //since_id: 954094550313766912
-        };
-
-        // min_id = Number.MAX_SAFE_INTEGER;
-        // var filtered_tweets = [];
-        // searchTweets(la_search).then(function(result) {
-        //     var tweets = result.statuses;
-        //     for (var i=0; i<tweets.length; i++){
-        //         var tweet = tweets[i];
-        //         var author = tweet.user;
-        //         if (author.followers_count > 100){
-        //             if (author.location.length > 0
-        //                 && author.location.toLowerCase().match(/^(|los angeles|new york|la|ny)$/)) {
-        //                 filtered_tweets.push(tweet);
-        //                 // retweet(tweet.id).then(function(result) {
-        //                 //
-        //                 // })
-        //             }
-        //         }
-        //     }
-        //     console.log(filtered_tweets);
-        //     console.log(min_id);
-        // }, function(err) {
-        //     console.log(err);
-        // });
-
-
-    } else {
-        console.log(err);
-    }
-});
+// getBearerToken(config.consumer_key, config.consumer_secret, function(err, response) {
+//     if(!err){
+//         config.bearer_token = response.body.access_token;
+//
+//         this.T = new Twitter(config);
+//
+//         var replyTo = {
+//             status: "👋",
+//             in_reply_to_status_id: 954498162902949889,
+//             auto_populate_reply_metadata: true
+//         }
+//         reply(replyTo).then(function(result) {
+//             console.log(result);
+//         });
+//
+//         var la_search = {
+//             q: 'roommate',
+//             count: 100,
+//             result_type: 'recent',
+//             lang: 'en',
+//             max_id: 954497154877358079
+//             // since_id: 9007199254740991
+//             //since_id: 954094550313766912
+//         };
+//
+//         // min_id = Number.MAX_SAFE_INTEGER;
+//         // var filtered_tweets = [];
+//         // searchTweets(la_search).then(function(result) {
+//         //     var tweets = result.statuses;
+//         //     for (var i=0; i<tweets.length; i++){
+//         //         var tweet = tweets[i];
+//         //         var author = tweet.user;
+//         //         if (author.followers_count > 100){
+//         //             if (author.location.length > 0
+//         //                 && author.location.toLowerCase().match(/^(|los angeles|new york|la|ny)$/)) {
+//         //                 filtered_tweets.push(tweet);
+//         //                 // retweet(tweet.id).then(function(result) {
+//         //                 //
+//         //                 // })
+//         //             }
+//         //         }
+//         //     }
+//         //     console.log(filtered_tweets);
+//         //     console.log(min_id);
+//         // }, function(err) {
+//         //     console.log(err);
+//         // });
+//
+//
+//     } else {
+//         console.log(err);
+//     }
+// });
 
 
 var reply  = function(params) {
@@ -65,7 +65,7 @@ var reply  = function(params) {
     return new Promise(function(resolve, reject) {
 
         // Do async job
-        this.T.post('statuses/retweet/', params, function(err, data, response) {
+        T.post('statuses/update', params, function(err, data, response) {
             if(!err){
                 var json = JSON.parse(response.body);
                 resolve(json);
@@ -84,7 +84,7 @@ var searchTweets = function(params) {
     return new Promise(function(resolve, reject) {
 
         // Do async job
-        this.T.get('search/tweets', params, function(err, data, response) {
+        T.get('search/tweets', params, function(err, data, response) {
             if(!err){
                 var json = JSON.parse(response.body);
                 resolve(json);
@@ -96,4 +96,28 @@ var searchTweets = function(params) {
         })
     })
 };
+
+T = new Twitter(config);
+
+var la_search = {
+    q: 'tech',
+    count: 2,
+    result_type: 'recent',
+    lang: 'en',
+    max_id: 954497154877358079
+    // since_id: 9007199254740991
+    //since_id: 954094550313766912
+};
+searchTweets(la_search).then(function(result) {
+    var tweet = result.statuses[0];
+    var replyTo = {
+        status: "👋",
+        in_reply_to_status_id: tweet.id_str,
+    };
+
+    reply(replyTo).then(function(result) {
+        console.log(result);
+    });
+
+});
 
