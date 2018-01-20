489f5bcb766fa99eb8f850b8b666cf2702a68f7e
Manish Dwibedy <manish.dwibedy@gmail.com>
2018-01-19 17:48:23 -0800

    searching with 'roommate'

--- test-twitter.js
+++ test-twitter.js
@@ -1,64 +1,9 @@
-const getBearerToken = require('get-twitter-bearer-token')
+const getBearerToken = require('get-twitter-bearer-token');
+var timediff = require('timediff');
 var Twitter = require('twitter');
-
 var config = require('./routes/config.js');
-var T = null;
 
-
-// getBearerToken(config.consumer_key, config.consumer_secret, function(err, response) {
-//     if(!err){
-//         config.bearer_token = response.body.access_token;
-//
-//         this.T = new Twitter(config);
-//
-//         var replyTo = {
-//             status: "👋",
-//             in_reply_to_status_id: 954498162902949889,
-//             auto_populate_reply_metadata: true
-//         }
-//         reply(replyTo).then(function(result) {
-//             console.log(result);
-//         });
-//
-//         var la_search = {
-//             q: 'roommate',
-//             count: 100,
-//             result_type: 'recent',
-//             lang: 'en',
-//             max_id: 954497154877358079
-//             // since_id: 9007199254740991
-//             //since_id: 954094550313766912
-//         };
-//
-//         // min_id = Number.MAX_SAFE_INTEGER;
-//         // var filtered_tweets = [];
-//         // searchTweets(la_search).then(function(result) {
-//         //     var tweets = result.statuses;
-//         //     for (var i=0; i<tweets.length; i++){
-//         //         var tweet = tweets[i];
-//         //         var author = tweet.user;
-//         //         if (author.followers_count > 100){
-//         //             if (author.location.length > 0
-//         //                 && author.location.toLowerCase().match(/^(|los angeles|new york|la|ny)$/)) {
-//         //                 filtered_tweets.push(tweet);
-//         //                 // retweet(tweet.id).then(function(result) {
-//         //                 //
-//         //                 // })
-//         //             }
-//         //         }
-//         //     }
-//         //     console.log(filtered_tweets);
-//         //     console.log(min_id);
-//         // }, function(err) {
-//         //     console.log(err);
-//         // });
-//
-//
-//     } else {
-//         console.log(err);
-//     }
-// });
-
+var T = new Twitter(config);
 
 var reply  = function(params) {
     // Return new promise
@@ -97,27 +42,55 @@ var searchTweets = function(params) {
     })
 };
 
-T = new Twitter(config);
 
 var la_search = {
-    q: 'tech',
-    count: 2,
+    q: 'roommate',
+    count: 100,
     result_type: 'recent',
     lang: 'en',
-    max_id: 954497154877358079
+    //max_id: 954497154877358079
     // since_id: 9007199254740991
     //since_id: 954094550313766912
 };
-searchTweets(la_search).then(function(result) {
-    var tweet = result.statuses[0];
-    var replyTo = {
-        status: "👋",
-        in_reply_to_status_id: tweet.id_str,
-    };
 
-    reply(replyTo).then(function(result) {
-        console.log(result);
-    });
 
+searchTweets(la_search).then(
+    function(result) {
+        var filtered_tweets = []
+        var tweets = result.statuses;
+        for (var i=0; i<tweets.length; i++){
+            var tweet = tweets[i];
+            var author = tweet.user;
+            timediff('2015-01-01', '2018-05-02 02:15:10.777', 'YDHms');
+
+            if (author.followers_count > 100){
+                if (author.location.length > 0
+                    && author.location.toLowerCase().match(/(los angeles|new york)/) != null) {
+                    filtered_tweets.push(tweet);
+
+                }
+            }
+        }
+        console.log(filtered_tweets);
+    }, function(err) {
+    console.log(err);
 });
+
+
+
+
+
+// searchTweets(la_search).then(function(result) {
+//     var tweet = result.statuses[0];
+//     var replyTo = {
+//         status: "👋",
+//         in_reply_to_status_id: tweet.id_str,
+//         auto_populate_reply_metadata: true
+//     };
+//
+//     reply(replyTo).then(function(result) {
+//         console.log(result);
+//     });
+//
+// });
 
